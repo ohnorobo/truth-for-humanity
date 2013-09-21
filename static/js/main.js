1132bdadf921de3c9425c1b927f14612d2cb8e93
cw wang <cwwang@gmail.com>
2013-09-21 18:37:43 -0400

    main js

--- static/js/main.js
+++ static/js/main.js
@@ -1,22 +1,20 @@
 
 $( document ).ready(function() {
-// Handler for .ready() called.
-var str = $( "body" ).text();
+	// Handler for .ready() called.
+	var str = $( "body" ).text();
 
-var words = str.split(" ");
-var count=words.length;
+	var words = str.split(" ");
+	var count=words.length;
 
-console.log(words.length);
-var randomNum = Math.ceil(Math.random()*count); 
-
-
-var searchString=words[randomNum];
-//str=str.replace(searchString,"<span style='backgrond-color:red'>"+searchString+"</span>");
-str=str.replace(searchString,"<span style='color:sienna;background-color:yellow;font-size:40px;'>"+searchString+"</span>");
-$( "body:last" ).html( str );
+	console.log(words.length);
+	var randomNum = Math.ceil(Math.random()*count); 
 
 
-
+	var searchString=words[randomNum];
+	//str=str.replace(searchString,"<span style='backgrond-color:red'>"+searchString+"</span>");
+	str=str.replace(searchString,"<span style='color:sienna;background-color:yellow;font-size:40px;'>"+searchString+"</span>");
+	
+	$( "body:last" ).html( str );
 
 });
 
