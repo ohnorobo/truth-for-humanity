6277554798fe2f020ba45710516dcec3c0af4d2e
cw wang <cwwang@gmail.com>
2013-09-21 17:07:20 -0400

    main.js update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 
 var txt=$("body").text;
-
 var wordCount = txt.replace( /[^\w ]/g, "" ).split( /\s+/ ).length
+var rand = Math.ceil(Math.random()*wordCount);
 
-
-var rand = Math.ceil(Math.random()*wordCount); /* Pick random number between 1 and 2 */
-
-$( "body" ).slice( 2 ).css( "background-color", "red" );
+$( "body" ).slice( rand ).css( "background-color", "red" );
