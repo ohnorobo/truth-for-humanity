61768d94a9bd988074e9a5253324b24c7f51af57
cw wang <cwwang@gmail.com>
2013-09-21 23:35:31 -0400

    more even distribution of images and css highlights

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,6 @@
 $( document ).ready(function() {
 
 	//h4 style
-	var colors=['red','yellow','cyan','orange'];
 	//$("h4").css("backgroundColor", colors[Math.floor(Math.random() * colors.length)]);
 	//$("h4").css("color", "red");
 	
@@ -16,24 +15,28 @@ $( document ).ready(function() {
 	console.log(searchString);
 	
 	var str=$("body").text();
-	var tCSS=['t1','t2','t3', 't4','t5','t6'];
+	var tCSS=['t1','t2','t3', 't4','t5','t6','t7','t8','t9','t10'];
 	
-	
-	var randCSS=tCSS[Math.floor(Math.random() * tCSS.length)];	
-	str=$("body").html().replace(searchString,"<span class='"+randCSS+"'>"+searchString+"</span>");
-	$( "body" ).html( str );
+	//highlight first word
+	// var randCSS=tCSS[Math.floor(Math.random() * tCSS.length)];	
+	// str=$("body").html().replace(searchString,"<span class='"+randCSS+"'>"+searchString+"</span>");
+	// $( "body" ).html( str );
 	
 
-
+	var colors=['red','yellow','cyan','orange'];
+	var rand=Math.floor(Math.random() * colors.length);
+	$( "body" ).css('color', colors[rand] );
 
 
 
 	//highlight random paragraph
-
-	for( var i=0; i<10; i++){
-		var pCSS=['p1','p2','p3','p4','p5'];
+	var pSize=$('p').size();
+	var r=Math.floor(Math.random() *pSize);
+	for( var i=0; i<pSize*.75; i++){
+		var pCSS=['p1','p2','p3','p4','p5','p6','p7','p8','p9'];
 		randCSS=pCSS[Math.floor(Math.random() * pCSS.length)];	
-		var random = Math.floor(Math.random() * 10 );
+		
+		var random = Math.floor(Math.random() * pSize );
 		$("p").eq(random).attr("class", randCSS);
 	}
 
@@ -61,10 +64,10 @@ $( document ).ready(function() {
 		
 		if(item.contains(unescape(searchString)))
 		{
-			console.log(item);
+			//console.log(item);
 		
 	 		randCSS=tCSS[Math.floor(Math.random() * tCSS.length)];	
-			console.log(item);
+			//console.log(item);
 			str=$("body").html().replace(item,"<span class='"+randCSS+"'>"+item+"</span>");
 			$( "body" ).html( str );
 		}
@@ -72,24 +75,26 @@ $( document ).ready(function() {
 
 
 	
+	//background based on search		
+		var randomBack=Math.round(Math.random()*10);
+			//set paragraph image
+		var	randomFirst=Math.round(Math.random()*10);
 	
-	
-	
-	
-	
-	
-	//background based on search
 	     $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
 	     {
 			       tags: unescape(searchString),
 			       tagmode: "all",
 			       format: "json",
-			is_getty:"true" //only getty images
+					//is_getty:"true" //only getty images
 			     },
 			     function(data) {
 			       $.each(data.items, function(i,item){
-			if ( i == 1){
-				$('body').css({'background-image': 'url('+ item.media.m + ')'});
+				
+				
+				
+
+			
+			if ( i == randomBack){
 				$('body').css({'background-image': 'url('+ item.media.m + ')'});
 	
 				var rand=1+Math.floor(Math.random() *10);
@@ -98,15 +103,15 @@ $( document ).ready(function() {
 				//background-size: 490px;
 			}
 			
-			//set paragraph image
-			else if ( i == 2){
-				var r=1+Math.floor(Math.random() *10);
-				
+		
+			else if ( i == randomFirst){
+				var r=0;
 				var img=$("<img/>").attr("src", item.media.m).prependTo("p:eq("+r+")");
 				img.css('width',400+'px');
 				img.css('padding','30px');
-				img.css('float','left');
-				
+				var rd=Math.round(Math.random());
+				if(rd==0) img.css('float','right');
+				else img.css('float','left');				
 			}
 			
 			else if ( i == 3){
@@ -115,14 +120,13 @@ $( document ).ready(function() {
 				var img=$("<img/>").attr("src", item.media.m).prependTo("p:eq("+r+")");
 				img.css('width',400+'px');
 				img.css('padding','30px');
-				img.css('float','right');
-				
+				var rd=Math.round(Math.random());
+				if(rd==0) img.css('float','right');
+				else img.css('float','left');				
 				randCSS=pCSS[Math.floor(Math.random() * pCSS.length)];	
 				img.parent().attr("class", randCSS);
 								
 			}
-			
-			
 			
 			else if ( i == 5){
 				var r=1+Math.floor(Math.random() *10);
@@ -130,23 +134,40 @@ $( document ).ready(function() {
 				var img=$("<img/>").attr("src", item.media.m).prependTo("p:eq("+r+")");
 				img.css('width',400+'px');
 				img.css('padding','30px');
-				img.css('float','right');
-				
+							var rd=Math.round(Math.random());
+							if(rd==0) img.css('float','right');
+							else img.css('float','left');				
 			}
-			
-
 			
 			else {
-		
 				//insert images
-				var img=$("<img/>").attr("src", item.media.m).prependTo("#results");
+				
+				var pSize=$('p').size();
+				
+				var r=1+Math.floor(Math.random() *pSize);
+				var img=$("<img/>").attr("src", item.media.m).prependTo("p:eq("+r+")");
+				
 				var rand=1+Math.floor(Math.random() *3);
-				img.css('width',100*rand+'px');
+				img.css('width',150*rand+'px');
+				
+				
+				var rd=Math.round(Math.random());
+				if(rd==0) img.css('float','right');
+				else img.css('float','left');
+				
 
 			}
 			
+			// else {
+			// 	//insert images
+			// 	var img=$("<img/>").attr("src", item.media.m).prependTo("#results");
+			// 	var rand=1+Math.floor(Math.random() *3);
+			// 	img.css('width',100*rand+'px');
+			// 
+			// }
+			
          
-			if ( i == 11)return false;
+			if ( i >= 100)return false;
 
 	       });
 	
