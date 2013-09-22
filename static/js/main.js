c904eee597cac9b6c6c8205628ac390e332f0438
cw wang <cwwang@gmail.com>
2013-09-21 20:55:43 -0400

    css js index

--- static/js/main.js
+++ static/js/main.js
@@ -3,8 +3,8 @@ $( document ).ready(function() {
 
 	//h4 style
 	var colors=['red','yellow','cyan','orange'];
-	$("h4").css("backgroundColor", colors[Math.floor(Math.random() * colors.length)]);
-	$("h4").css("color", "red");
+	//$("h4").css("backgroundColor", colors[Math.floor(Math.random() * colors.length)]);
+	//$("h4").css("color", "red");
 	
 	
 	
@@ -16,40 +16,67 @@ $( document ).ready(function() {
 	console.log(searchString);
 	
 	var str=$("body").text();
+	var tCSS=['t1','t2','t3', 't4','t5'];
 	
-	var tCSS=['t1','t2','t3'];
+	
 	var randCSS=tCSS[Math.floor(Math.random() * tCSS.length)];	
 	str=$("body").html().replace(searchString,"<span class='"+randCSS+"'>"+searchString+"</span>");
-
 	$( "body:last" ).html( str );
 	
 
-	//background based on search
-	        $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-	        {
-	          tags: searchString,
-	          tagmode: "all",
-	          format: "json",
-				is_getty:"true" //only getty images
-	        },
-	        function(data) {
-	          $.each(data.items, function(i,item){
-				if ( i == 1){
-					$('body').css({'background-image': 'url('+ item.media.m + ')'});
-					$('body').css({'background-image': 'url('+ item.media.m + ')'});
-					
-					//background-size: 490px;
-				}
-				else {
-		            var img=$("<img/>").attr("src", item.media.m).prependTo("#results");
-					var rand=1+Math.floor(Math.random() *3);
-		 			img.css('width',100*rand+'px');
-		
-				}
-	            
-				if ( i == 11)return false;
+	//highlight sentences
+	cleanText = str.replace(/<\/?[^>]+(>|$)/g, "");
+	var sentences=cleanText.split(".");
 	
-	          });
+	randCSS=tCSS[Math.floor(Math.random() * tCSS.length)];	
+	console.log(sentences[1]);
+	str=$("body").html().replace(sentences[1],"<span class='"+randCSS+"'>"+sentences[1]+"</span>");
+	$( "body:last" ).html( str );
+	
+	randCSS=tCSS[Math.floor(Math.random() * tCSS.length)];	
+	console.log(sentences[6]);
+	str=$("body").html().replace(sentences[6],"<span class='"+randCSS+"'>"+sentences[6]+"</span>");
+	$( "body:last" ).html( str );
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//background based on search
+	     $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+	     {
+	       tags: unescape(searchString),
+	       tagmode: "all",
+	       format: "json",
+	is_getty:"true" //only getty images
+	     },
+	     function(data) {
+	       $.each(data.items, function(i,item){
+	if ( i == 1){
+		$('body').css({'background-image': 'url('+ item.media.m + ')'});
+		$('body').css({'background-image': 'url('+ item.media.m + ')'});
+	
+		var rand=1+Math.floor(Math.random() *10);
+		$('body').css({'background-size': 100*rand+'px'});
+	
+		//background-size: 490px;
+	}
+	else {
+	          var img=$("<img/>").attr("src", item.media.m).prependTo("#results");
+		var rand=1+Math.floor(Math.random() *3);
+			img.css('width',100*rand+'px');
+
+	}
+         
+	if ( i == 11)return false;
+
+	       });
 	
 	
 	//console.log(data.items);
